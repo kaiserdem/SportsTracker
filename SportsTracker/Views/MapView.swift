72ac9d200682c7777421e615b910f5e6ebc1ecf3
Yaroslav <yaroslavgolinsiy@gmail.com>
2025-10-02 20:54:28 +0200

    no message

--- SportsTracker/Views/MapView.swift
+++ SportsTracker/Views/MapView.swift
@@ -7,27 +7,52 @@ struct MapView: View {
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            Map(
-                coordinateRegion: Binding(
-                    get: {
-                        if let location = viewStore.userLocation {
-                            return MKCoordinateRegion(
-                                center: location.coordinate,
-                                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                            )
-                        } else {
-                            return MKCoordinateRegion(
-                                center: CLLocationCoordinate2D(latitude: 50.4501, longitude: 30.5234),
-                                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                            )
+            ZStack {
+                Map(
+                    coordinateRegion: Binding(
+                        get: {
+                            if let location = viewStore.userLocation {
+                                return MKCoordinateRegion(
+                                    center: location.coordinate,
+                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                )
+                            } else {
+                                // Просто встановлюємо карту в центр карти без хардкоду конкретних координат
+                                return MKCoordinateRegion(
+                                    center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+                                    span: MKCoordinateSpan(latitudeDelta: 100, longitudeDelta: 100)
+                                )
+                            }
+                        },
+                        set: { _ in }
+                    ),
+                    showsUserLocation: true,
+                    userTrackingMode: .none
+                )
+                .ignoresSafeArea()
+                
+                // Кнопка центрування на поточну точку (завжди видима)
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // Одноразове центрування на поточну точку
+                            viewStore.send(.getCurrentLocation)
+                        }) {
+                            Image(systemName: "location.fill")
+                                .font(.system(size: 20, weight: .medium))
+                                .foregroundColor(.white)
+                                .frame(width: 48, height: 48)
+                                .background(Theme.Palette.primary)
+                                .clipShape(Circle())
+                                .shadow(color: .black.opacity(0.3), radius: 8, x: 0, y: 4)
                         }
-                    },
-                    set: { _ in }
-                ),
-                showsUserLocation: true,
-                userTrackingMode: .none
-            )
-            .ignoresSafeArea()
+                        .padding(.trailing, 20)
+                        .padding(.bottom, 100) // Відступ від нижнього краю
+                    }
+                }
+            }
             .onAppear {
                 viewStore.send(.onAppear)
             }
